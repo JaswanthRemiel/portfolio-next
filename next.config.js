25ed68fff9076871a85f60db99135f8aed56b404
Jaswanth Remiel AG <jaswanthremiel7@gmail.com>
2024-11-25 17:36:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  output: 'export', // Generates static files for GitHub Pages
+  basePath: '/portfolio-next', // Replace <repository-name> with the name of your GitHub repository
+  trailingSlash: true, // Ensures paths have trailing slashes for GitHub Pages compatibility
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
